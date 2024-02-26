290d019b5ac71a46a8c627bfc1e7a936603de886
1chooo <hugo970217@gmail.com>
2024-02-26 21:59:41 +0800

    add: C++ HelloWorld templates

--- templates/build.sh
+++ templates/build.sh
@@ -1,1 +1,8 @@
-g++ -Iinclude src/*.cpp -o bin/main.out && ./bin/main.out
+# compile main program
+g++ -Iinclude src/main.cpp src/HelloWorld.cpp -o bin/main.out
+
+# compile test program
+g++ -Iinclude test/test_HelloWorld.cpp src/HelloWorld.cpp -o bin/test.out
+
+# run test program
+./bin/test.out
